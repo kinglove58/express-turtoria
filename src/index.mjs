5eb710d6c4adec4c7fd746ba87bc55e31888b185
kinglove58 <obafemielijahsunday@gmail.com>
2024-12-10 20:52:15 +0100

    Add cookie-parser and express-session middleware for session management

--- src/index.mjs
+++ src/index.mjs
@@ -2,9 +2,22 @@ import express from "express";
 import { mockData } from "./utils/constant.mjs";
 import { resolvedIndexByUser } from "./utils/middlewares.mjs";
 import router from "./routers/index.mjs";
+import cookieParser from "cookie-parser";
+import session from "express-session";
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser("hello world"));
+app.use(
+  session({
+    secret: "elijah techking",
+    saveUninitialized: false,
+    resave: false,
+    cookie:{
+      maxAge: 6000 * 60,
+    }
+  })
+);
 app.use(router);
 
 const PORT = process.env.PORT || 3000;
@@ -18,6 +31,14 @@ app.get("/api/user/:id", resolvedIndexByUser, (req, res) => {
   const findUser = mockData[findUserId];
   if (!findUser) return res.sendStatus(404);
   return res.send(findUser);
+});
+
+app.get("/", (req, res) => {
+  console.log(req.session);
+  console.log(req.sessionID);
+  
+  res.cookie("jesus is lord", { maxAge: 6000 * 60 * 2 });
+  res.status(201).send({ msg: "Hello World!", signed: true });
 });
 
 app.listen(PORT, () => {
--- src/routers/products.mjs
+++ src/routers/products.mjs
@@ -3,12 +3,19 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/api/products", (req, res) => {
-  res.send([
-    {
-      id: 123,
-      name: "chicken",
-      price: 3000,
-    },
-  ]);
+  console.log(req.headers.cookie);
+  console.log(req.cookies);
+  console.log(req.signedCookies)
+
+  if (req.cookies.hello && req.cookies.hello === "world")
+    return res.send([
+      {
+        id: 123,
+        name: "chicken",
+        price: 3000,
+      },
+    ]);
+
+  return res.send({ msg: "am sorry no cookies" });
 });
 export default router;
